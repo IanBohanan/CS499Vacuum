b77bc849cd2b4f07bb7e40912b8eb28a2e897bf0
Phil-Eggtree <icb0003@uah.edu>
2023-08-30 19:17:17 -0500

    Init Commit
    Example scene has grid and vacuum

---
+++ Assets/Scripts/VacuumMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VacuumMovement : MonoBehaviour
+{
+    float speed = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += new Vector3(speed, 0, 0);
+        float startpoint = -10.1f;
+        if(this.transform.position.x >= 10)
+        {
+            //speed *= -1;
+        }
+        if(this.transform.position.x <= -10)
+        {
+            //speed *= -1;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Reclaulate trjectory using the given aglorithm
+        print("Vacuum collided");
+        speed *= -1;
+    }
+}
