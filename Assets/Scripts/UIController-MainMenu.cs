78eb278d604649e792eca5057ceeb4f09b95b69d
George Coppel <georgelcoppe@gmail.com>
2023-09-13 12:44:56 -0500

    Added import menu popup that will be used for selecting a JSON file to use.

--- Assets/Scripts/UIController-MainMenu.cs
+++ Assets/Scripts/UIController-MainMenu.cs
@@ -9,24 +9,36 @@ public class MainMenuController : MonoBehaviour
     Button importBtn;
     Button createNewBtn;
 
+    VisualElement importPopup;
+    Button importJSONButton;
+
     void OnEnable()
     {
         // Get UIDocument Root:
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
 
-        // Get Button Components:
+        // Get Primary Button Components:
         VisualElement buttonContainer = root.Q<VisualElement>("ButtonContainer");
         Button importBtn = buttonContainer.Q<Button>("ImportButton");
         Button createNewBtn = buttonContainer.Q<Button>("CreateNewButton");
 
+        // Get Import Popup Panel & Button:
+        importPopup = root.Q<VisualElement>("ImportPopup");
+        importJSONButton = importPopup.Q<Button>("ImportJSONButton");
+
         // Subscribe to button events:
         importBtn.clicked += () => onImportPress();
         createNewBtn.clicked += () => onCreateNewPress();
+        importJSONButton.clicked += () => onImportPopupPress();
     }
 
     // Open the JSON import panel
     public void onImportPress() {
-        Debug.Log("Import JSON");
+        importPopup.style.display = DisplayStyle.Flex;
+    }
+
+    public void onImportPopupPress(){
+        importPopup.style.display = DisplayStyle.None;
     }
 
     // Load in the house creation scene
