15b5d3936a910b3d306073f321535761cf8ca200
Khushal Noori <kn0045@uah.edu>
2023-11-07 12:16:19 -0600

    Update CameraMovement.cs

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,24 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//This script controls the camera's movement and zoom in the game 
 public class CameraMovement : MonoBehaviour
 {
-
+    // Variable to store the camera's position.
     private Vector3 CameraPostion;
+    // Reference to the Camera component.
     private Camera cam;
-
+    // Inspector-exposed settings for the camera.
     [Header("Camera Settings")]
-    public float cameraSpeed; //How fast the camera speed is
-    private float zoom;
-    private float zoomMultiplier = 4f;
-    private float minZoom = 20f;
-    private float maxZoom = 80f;
-    private float velocity = 0f;
-    private float smoothTime = 0.25f;
-
+    public float cameraSpeed; // The speed at which the camera moves.
+    private float zoom; // The current zoom level of the camera.
+    private float zoomMultiplier = 4f; // How much the zoom changes with input.
+    private float minZoom = 20f; // Minimum allowed zoom level.
+    private float maxZoom = 80f; // Maximum allowed zoom level.
+    private float velocity = 0f; // Used by SmoothDamp to store the current velocity.
+    private float smoothTime = 0.25f; // Time taken to smooth the zoom transition.
+    
+    // Start is called before the first frame update
     private void Start()
     {
+        // Initialize the camera's position and the zoom level.
         CameraPostion = transform.position;
         cam = GetComponent<Camera>();
         zoom = cam.orthographicSize;
