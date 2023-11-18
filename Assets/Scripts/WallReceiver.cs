1667b08ce2cb200975cee3f04a774882427aed01
Khushal Noori <kn0045@uah.edu>
2023-11-18 12:33:32 -0600

    Update WallReceiver.cs

--- Assets/Scripts/WallReceiver.cs
+++ Assets/Scripts/WallReceiver.cs
@@ -2,32 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Subscribes to stateUpdate from the HouseBuilderUI
-//If the user is trying to place a wall, it updates these currently placed walls to allow user to click on the endpoints
-//and attach another wall to the end
-
+// The WallReceiver class subscribes to state updates from the HouseBuilderUI.
+// It enables or disables the wall UI based on whether the user is in wall placement mode.
 public class WallReceiver : MonoBehaviour
 {
+    public GameObject wallUI; // The UI GameObject that contains the pivot and spawner for wall placement.
 
-    public GameObject wallUI; //The wallUI gameobject that contains the pivot and spawner
-
-    //When object created (and enabled) subscribe to the UI's stateUpdate action
+    // Subscribe to the HouseBuilderUI's stateUpdate action when the GameObject is enabled.
+    // This is important for dynamically updating the UI based on the user's current action in the house builder.
     private void OnEnable()
     {
         HouseBuilderUI.stateUpdate += stateUpdated;
     }
 
-    //When object created (and enabled) unsubscribe to the UI's stateUpdate action. 
-    //Very important or else errors will happen!
+    // Unsubscribe from the HouseBuilderUI's stateUpdate action when the GameObject is disabled.
+    // This is crucial to prevent errors due to trying to update UI on a disabled or destroyed object.
     private void OnDisable()
     {
         HouseBuilderUI.stateUpdate -= stateUpdated;
     }
 
-    //Triggered whenever the UI state changes
+    // This method is triggered whenever the UI state changes.
+    // It activates or deactivates the wall UI based on the current state of the UI, particularly checking for "WallPlacement" state.
     private void stateUpdated(string newState)
     {
-        if(newState == "WallPlacement")
+        // Enable the wall UI if the new state is WallPlacement, otherwise disable it.
+        if (newState == "WallPlacement")
         {
             wallUI.SetActive(true);
         }
@@ -35,6 +35,7 @@ public class WallReceiver : MonoBehaviour
         {
             wallUI.SetActive(false);
         }
+        // Uncomment the below line for debugging purposes to print the current state.
         //print("WallReceiver: The UI's new state was set to " + newState);
     }
 }
