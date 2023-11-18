cb85171d24a040e1973647bfeebd717cb3e9a996
Khushal Noori <kn0045@uah.edu>
2023-11-18 12:15:28 -0600

    Update TileData.cs

--- Assets/Scripts/TileData.cs
+++ Assets/Scripts/TileData.cs
@@ -5,12 +5,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+// This attribute allows the creation of TileData assets from the Unity Editor
 [CreateAssetMenu]
 public class TileData : ScriptableObject
 {
-    public TileBase[] tiles;
+    // Array to hold references to different tile types
+    public TileBase[] tiles; // Tiles associated with this data
 
-    public float cleanliness;
+    // Cleanliness value for the tile, can be used to store how clean/dirty a tile is
+    public float cleanliness; // Float to represent the cleanliness level of the tile
 
-    enum Type { HARD, LOOP, CUT, FREEZECUT };
+    // Enumeration to define different types of tiles
+    enum Type { HARD, LOOP, CUT, FREEZECUT }; // Types of tiles (e.g., different flooring types or characteristics)
 }
