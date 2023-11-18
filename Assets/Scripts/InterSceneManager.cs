ebf14f28ce9115e1f2f022db69dec830a1d2fa9d
Khushal Noori <kn0045@uah.edu>
2023-11-18 12:13:48 -0600

    Update InterSceneManager.cs

--- Assets/Scripts/InterSceneManager.cs
+++ Assets/Scripts/InterSceneManager.cs
@@ -2,45 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This class is used to manage data across different scenes in a Unity game
 public static class InterSceneManager
 {
-    // Main Menu Import File:
+    // Variable to store file selection from Main Menu
     public static string fileSelection = "";
 
-    // House Builder UI -> ClickDrop
+    // Flag to toggle delete mode in House Builder UI
     public static bool deleteMode = false;
 
-    // Simulation Setup Settings:
-    private static bool whiskersEnabled;
-    private static string floorCovering;
-    private static int batteryLife;
-    private static bool randomAlgEnabled;
-    private static bool spiralAlgEnabled;
-    private static bool snakingAlgEnabled;
-    private static bool wallFollowAlgEnabled;
+    // Private variables to store Simulation Setup Settings:
+    private static bool whiskersEnabled; // Flag for enabling whiskers
+    private static string floorCovering; // Type of floor covering
+    private static int batteryLife; // Duration of battery life
+    private static bool randomAlgEnabled; // Flag for enabling random algorithm
+    private static bool spiralAlgEnabled; // Flag for enabling spiral algorithm
+    private static bool snakingAlgEnabled; // Flag for enabling snaking algorithm
+    private static bool wallFollowAlgEnabled; // Flag for enabling wall following algorithm
 
-    // Simulation Speed, used by Vacuum to multiply speed:
-    public static int speedMultiplier = 1;
-        
+    // Public variable to adjust simulation speed
+    public static int speedMultiplier = 1; // Multiplier for simulation speed
+    
+    // Method to set simulation settings
     public static void setSimulationSettings(bool whiskers, string floorCov, int battery, bool randomAlg, bool spiralAlg, bool snakingAlg, bool wallFollowAlg)
     {
-        whiskersEnabled = whiskers;
-        floorCovering = floorCov;
-        batteryLife = battery; 
-        randomAlgEnabled = randomAlg;
-        spiralAlgEnabled = spiralAlg;
-        snakingAlgEnabled = snakingAlg;
-        wallFollowAlgEnabled = wallFollowAlg;
-        return;
+        whiskersEnabled = whiskers; // Set whiskers flag
+        floorCovering = floorCov; // Set floor covering type
+        batteryLife = battery; // Set battery life
+        randomAlgEnabled = randomAlg; // Set random algorithm flag
+        spiralAlgEnabled = spiralAlg; // Set spiral algorithm flag
+        snakingAlgEnabled = snakingAlg; // Set snaking algorithm flag
+        wallFollowAlgEnabled = wallFollowAlg; // Set wall following algorithm flag
     }
 
+    // Method to get the status of path algorithms
     public static (bool, bool, bool, bool) getPathAlgs()
     {
+        // Return the status of each pathfinding algorithm
         return (randomAlgEnabled, spiralAlgEnabled, snakingAlgEnabled, wallFollowAlgEnabled);
     }
 
+    // Method to get current simulation settings
     public static (bool, string, int) getSimulationSettings()
     {
+        // Return the whiskers enabled flag, floor covering type, and battery life
         return (whiskersEnabled, floorCovering, batteryLife);
     }
 }
