55efc7671dc807dd01fd1ed54214d25337c22bd2
Ian-Bohanan <icb0003@uah.edu>
2023-11-26 15:39:04 -0600

    comments

--- Assets/Scripts/Flag.cs
+++ Assets/Scripts/Flag.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+//The flag for each room. Used in the housebuilder scene to make sure every room is accessible.
+//It denotes a space that's in each room
 public class Flag : MonoBehaviour
 {
 
