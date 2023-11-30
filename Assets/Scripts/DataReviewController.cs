e6b445dcef4d057a377d57915cfda0383b52768e
Khushal Noori <kn0045@uah.edu>
2023-11-29 20:09:40 -0600

    Update DataReviewController.cs

--- Assets/Scripts/DataReviewController.cs
+++ Assets/Scripts/DataReviewController.cs
@@ -4,71 +4,67 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
-using static LayoutManager;
-// Define serializable classes to be used for parsing JSON data
+using static LayoutManager; // Import a LayoutManager class, possibly from another script or package.
+
 #region JSON Serializable Classes
 [Serializable]
 public class Settings
 {
-    public bool whiskers;
-    public string floorCovering;
-    public int batteryLifeStart;
+    public bool whiskers; // Boolean flag for whiskers setting.
+    public string floorCovering; // String representing the floor covering type.
+    public int batteryLifeStart; // Starting battery life value.
 }
 
 [Serializable]
 public class RandomData
 {
-    public int elapsedTime;
-    public int batteryLifeEnd;
-    public int cleaningEfficiency;
-    public int tilesCleaned;
-    public int untouchedTiles;
+    public int elapsedTime; // Elapsed time data.
+    public int batteryLifeEnd; // Ending battery life data.
+    public int cleaningEfficiency; // Cleaning efficiency data.
+    public int tilesCleaned; // Number of tiles cleaned data.
+    public int untouchedTiles; // Number of untouched tiles data.
 }
 
 [Serializable]
 public class SimulationEntry
 {
-    public Settings Settings;
-    public RandomData Random;
-    public RandomData WallFollow;
-    public RandomData Spiral;
-    public RandomData Snaking;
+    public Settings Settings; // Settings for a simulation run.
+    public RandomData Random; // Random data for a simulation run.
+    public RandomData WallFollow; // Wall follow data for a simulation run.
+    public RandomData Spiral; // Spiral data for a simulation run.
+    public RandomData Snaking; // Snaking data for a simulation run.
 }
 
 [Serializable]
 public class RootObject<T>
 {
-    public List<T> SIMULATION_DATA;
+    public List<T> SIMULATION_DATA; // List of simulation entries.
 }
 #endregion
 
-// Controller class for reviewing data within the Unity Engine
 public class DataReviewController : MonoBehaviour
 {
-    int currentRun = 0; // Index of the current simulation run being reviewed
-    string jsonFile = "Layout1"; // The default JSON file name that contains simulation data
-    
-     // UI Elements that will be interacted with in the scene
+    int currentRun = 0; // The currently viewed run index.
+    string jsonFile = "Layout1"; // The JSON file to load data from.
+
     TreeView data;
     Button returnToMainMenu;
     Button prevBtn;
     Button nextBtn;
 
-    // Fields for displaying JSON data for the currently viewed run
-    // These are not used in the current script and can be removed or implemented for additional features.
-    int runNumber;
-    bool whiskers;
-    string floorCovering;
-    float batteryLifeStart;
-    // List<RunData> algorithmRuns = new List<RunData>();
+    // JSON Data for Currently Viewed Run:
+    int runNumber; // The run number (not used in this script).
+    bool whiskers; // Whiskers setting for the current run (not used in this script).
+    string floorCovering; // Floor covering type for the current run (not used in this script).
+    float batteryLifeStart; // Starting battery life for the current run (not used in this script).
+    // List<RunData> algorithmRuns = new List<RunData>(); // Commented out code, possibly not used.
 
-    // Called when the script is loaded and the GameObject it is attached to is activated
     void OnEnable()
     {
-        // Access the UI Document and retrieve the root visual element
+        // Get UIDocument Root:
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
 
-        // Initialize UI elements by finding them by name in the UI hierarchy
+        // Get top panel items:
         returnToMainMenu = root.Q<Button>("GoHomeButton");
 
         // Get bottom panel items:
@@ -77,24 +73,20 @@ public class DataReviewController : MonoBehaviour
 
         // Get Data Panel Items:
         data = root.Q<TreeView>("Data");
-        // Setup callback functions for button interactions
+
         subscribeToCallbacks();
 
-        // Load and display the initial data onto the UI
         fillDataPanel();
     }
 
-    // Method for setting up callbacks for button interactions
+    // Subscribe to function callbacks:
     private void subscribeToCallbacks()
     {
-        // When the main menu button is clicked, load the main menu scene
         returnToMainMenu.clicked += () => { SceneManager.LoadScene(sceneName: "MainMenu"); };
-        // When the previous button is clicked, show the previous simulation run data
         prevBtn.clicked += () => { prevButtonHandler(); };
-        // When the next button is clicked, show the next simulation run data
         nextBtn.clicked += () => { nextButtonHandler(); };
     }
-    // Handler for the previous button; decrement the current run index and update the UI
+
     private void prevButtonHandler()
     {
         if (currentRun > 0)
@@ -104,12 +96,14 @@ public class DataReviewController : MonoBehaviour
             fillDataPanel();
         }
     }
-    // Handler for the next button; increment the current run index and update the UI
+
     private void nextButtonHandler()
-    {   // Read the JSON file containing simulation data
+    {
+        // Read and parse JSON data from a file.
         string unparsedJSON = System.IO.File.ReadAllText(Application.dataPath + "/StreamingAssets/" + jsonFile + ".json");
         RootObject<SimulationEntry> parsedJSON = JsonUtility.FromJson<RootObject<SimulationEntry>>(unparsedJSON);
-        // If not at the end of the list, increment the current run and update the UI
+
+        // Check if there are more runs to navigate to.
         if (currentRun < parsedJSON.SIMULATION_DATA.Count - 1)
         {
             currentRun++;
@@ -118,29 +112,29 @@ public class DataReviewController : MonoBehaviour
         }
     }
 
-    /// Clears all data from the TreeView UI component
+    // Remove all elements from the data TreeView:
     private void clearDataPanel()
     {
         data.hierarchy.ElementAt(0).Clear();
     }
 
-    // Populates the TreeView with the data from the current simulation run
+    // Load the data TreeView with a new run's info:
     private void fillDataPanel()
     {
-        // Read the JSON file again to refresh the data
+        // Read and parse JSON data from a file.
         string unparsedJSON = System.IO.File.ReadAllText(Application.dataPath + "/StreamingAssets/" + jsonFile + ".json");
         RootObject<SimulationEntry> parsedJSON = JsonUtility.FromJson<RootObject<SimulationEntry>>(unparsedJSON);
-        // Add UI elements to the TreeView for each piece of data in the simulation run
-        // For each category (Settings, Random, WallFollow, Spiral, Snaking), add headers and corresponding values
-        AddRunNumberLabel("Run Number: "+(currentRun+1));
-        // ...
-        // (Additional UI elements are added for each section of the simulation data)
-        // ...
-        AddNewSectionHeader("\nSettings:");
-        AddLabelToData("Whiskers: "+parsedJSON.SIMULATION_DATA[currentRun].Settings.whiskers);
-        AddLabelToData("Floor Covering: "+parsedJSON.SIMULATION_DATA[currentRun].Settings.floorCovering);
-        AddLabelToData("Starting Battery Life: "+parsedJSON.SIMULATION_DATA[currentRun].Settings.batteryLifeStart);
 
+        // Add run number label to the data panel.
+        AddRunNumberLabel("Run Number: " + (currentRun + 1));
+
+        // Add settings section with relevant data.
+        AddNewSectionHeader("\nSettings:");
+        AddLabelToData("Whiskers: " + parsedJSON.SIMULATION_DATA[currentRun].Settings.whiskers);
+        AddLabelToData("Floor Covering: " + parsedJSON.SIMULATION_DATA[currentRun].Settings.floorCovering);
+        AddLabelToData("Starting Battery Life: " + parsedJSON.SIMULATION_DATA[currentRun].Settings.batteryLifeStart);
+
+        // Add random data section with relevant data.
         AddNewSectionHeader("\nRandom:");
         AddLabelToData("Elapsed Time: " + parsedJSON.SIMULATION_DATA[currentRun].Random.elapsedTime);
         AddLabelToData("Ending Battery Life: " + parsedJSON.SIMULATION_DATA[currentRun].Random.batteryLifeEnd);
@@ -148,7 +142,7 @@ public class DataReviewController : MonoBehaviour
         AddLabelToData("Tiles Cleaned: " + parsedJSON.SIMULATION_DATA[currentRun].Random.tilesCleaned);
         AddLabelToData("Untouched Tiles: " + parsedJSON.SIMULATION_DATA[currentRun].Random.untouchedTiles);
 
-
+        // Add wall follow data section with relevant data.
         AddNewSectionHeader("\nWall Follow:");
         AddLabelToData("Elapsed Time: " + parsedJSON.SIMULATION_DATA[currentRun].WallFollow.elapsedTime);
         AddLabelToData("Ending Battery Life: " + parsedJSON.SIMULATION_DATA[currentRun].WallFollow.batteryLifeEnd);
@@ -156,6 +150,7 @@ public class DataReviewController : MonoBehaviour
         AddLabelToData("Tiles Cleaned: " + parsedJSON.SIMULATION_DATA[currentRun].WallFollow.tilesCleaned);
         AddLabelToData("Untouched Tiles: " + parsedJSON.SIMULATION_DATA[currentRun].WallFollow.untouchedTiles);
 
+        // Add spiral data section with relevant data.
         AddNewSectionHeader("\nSpiral:");
         AddLabelToData("Elapsed Time: " + parsedJSON.SIMULATION_DATA[currentRun].Spiral.elapsedTime);
         AddLabelToData("Ending Battery Life: " + parsedJSON.SIMULATION_DATA[currentRun].Spiral.batteryLifeEnd);
@@ -163,6 +158,7 @@ public class DataReviewController : MonoBehaviour
         AddLabelToData("Tiles Cleaned: " + parsedJSON.SIMULATION_DATA[currentRun].Spiral.tilesCleaned);
         AddLabelToData("Untouched Tiles: " + parsedJSON.SIMULATION_DATA[currentRun].Spiral.untouchedTiles);
 
+        // Add snaking data section with relevant data.
         AddNewSectionHeader("\nSnaking:");
         AddLabelToData("Elapsed Time: " + parsedJSON.SIMULATION_DATA[currentRun].Snaking.elapsedTime);
         AddLabelToData("Ending Battery Life: " + parsedJSON.SIMULATION_DATA[currentRun].Snaking.batteryLifeEnd);
@@ -170,32 +166,32 @@ public class DataReviewController : MonoBehaviour
         AddLabelToData("Tiles Cleaned: " + parsedJSON.SIMULATION_DATA[currentRun].Snaking.tilesCleaned);
         AddLabelToData("Untouched Tiles: " + parsedJSON.SIMULATION_DATA[currentRun].Snaking.untouchedTiles);
     }
-    // Helper method to create and add a label for the run number in the UI
+
     private void AddRunNumberLabel(string text)
     {
+        // Add a label for the run number.
         Label newHeader = new Label(text);
-        // Style the label
         newHeader.style.fontSize = 18;
         newHeader.style.color = Color.white;
         newHeader.style.unityFontStyleAndWeight = FontStyle.Bold;
         newHeader.style.alignSelf = Align.Center;
         data.hierarchy.ElementAt(0).Add(newHeader);
     }
-    // Helper method to create and add section headers in the UI
+
     private void AddNewSectionHeader(string text)
     {
+        // Add a header for a new section.
         Label newHeader = new Label(text);
-        // Style the label
         newHeader.style.fontSize = 16;
         newHeader.style.color = Color.white;
         newHeader.style.unityFontStyleAndWeight = FontStyle.Bold;
         data.hierarchy.ElementAt(0).Add(newHeader);
     }
-    // Helper method to create and add general data labels in the UI
+
     private void AddLabelToData(string text)
     {
+        // Add a label with data to the data panel.
         Label newLabel = new Label(text);
-        // Style the label
         newLabel.style.fontSize = 12;
         newLabel.style.color = Color.white;
         newLabel.style.marginLeft = 50;
