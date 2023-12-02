127838cee7055579171f0a69a8a5d358ad7e702e
Khushal Noori <kn0045@uah.edu>
2023-12-02 11:58:39 -0600

    Update CameraMovement.cs

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,61 +1,54 @@
+// This script allows for camera movement and zoom control in a Unity 2D game.
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//This script controls the camera's movement and zoom in the game 
+
 public class CameraMovement : MonoBehaviour
 {
-    // Variable to store the camera's position.
-    private Vector3 CameraPostion;
-    // Reference to the Camera component.
-    private Camera cam;
-    // Inspector-exposed settings for the camera.
+    // Variables for camera movement and zoom control
+    private Vector3 CameraPostion;  // Stores the current camera position
+    private Camera cam;             // Reference to the camera component
+
     [Header("Camera Settings")]
-    public float cameraSpeed; // The speed at which the camera moves.
-    private float zoom; // The current zoom level of the camera.
-    private float zoomMultiplier = 4f; // How much the zoom changes with input.
-    private float minZoom = 20f; // Minimum allowed zoom level.
-    private float maxZoom = 80f; // Maximum allowed zoom level.
-    private float velocity = 0f; // Used by SmoothDamp to store the current velocity.
-    private float smoothTime = 0.25f; // Time taken to smooth the zoom transition.
-    
-    // Start is called before the first frame update
+    public float cameraSpeed;       // Speed of camera movement
+    private float zoom;             // Current camera zoom level
+    private float zoomMultiplier = 4f; // Multiplier for zoom sensitivity
+    private float minZoom = 20f;    // Minimum allowed zoom level
+    private float maxZoom = 80f;    // Maximum allowed zoom level
+    private float velocity = 0f;    // Velocity for smooth zooming
+    private float smoothTime = 0.25f; // Smoothness factor for zooming
+
     private void Start()
     {
-        // Initialize the camera's position and the zoom level.
+        // Initialize camera position and get a reference to the Camera component
         CameraPostion = transform.position;
         cam = GetComponent<Camera>();
         zoom = cam.orthographicSize;
-
     }
-    // Method to handle zoom input and apply the zoom effect.
+
     private void getZoom()
-    {   
-        // Get the scroll wheel input.
+    {
+        // Adjust the camera's orthographic size (zoom) based on mouse scroll input
         float scroll = Input.GetAxis("Mouse ScrollWheel");
-        // Adjust the zoom level based on input, multiplied by the zoomMultiplier.
         zoom -= scroll * zoomMultiplier;
-        // Clamp the zoom level to the min and max bounds.
         zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
-        // Smoothly transition to the new zoom level.
+        // Smoothly transition to the new zoom level
         cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, zoom, ref velocity, smoothTime);
     }
-    // Update is called once per frame.
+
     private void Update()
-    {   
-        //Initialize a new Vector3 to store movement input
+    {
+        // Capture user input for camera movement
         Vector3 movement;
-        // Get horizontal and vertical input and multiply by half of the cameraSpeed.
-        movement.x = Input.GetAxisRaw("Horizontal") * (cameraSpeed*0.5f);
-        movement.y = Input.GetAxisRaw("Vertical") * (cameraSpeed*0.5f);
+        movement.x = Input.GetAxisRaw("Horizontal") * (cameraSpeed * 0.5f);
+        movement.y = Input.GetAxisRaw("Vertical") * (cameraSpeed * 0.5f);
         movement.z = 0;
-        // Uncomment the following line if you want to ensure consistent movement speed in all directions.
-        //movement = movement.normalized; // Normalize the movement vector so diagonal movement isn't faster.
-        // Update the camera's position based on the movement input.
+        // Update the camera position based on user input
         CameraPostion += movement;
-        // Apply the updated position to the camera's transform.
         transform.position = CameraPostion;
-        // Call the getZoom method to handling the zooming
+
+        // Call the getZoom() function to handle zooming based on mouse scroll input
         getZoom();
     }
-
 }
