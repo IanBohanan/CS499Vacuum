17fd21789e82581df6fba9d4876c1479a9cdc92f
MEMEMEME <csvkes@gmail.com>
2023-12-02 12:46:48 -0600

    Room Dimension Check added

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -148,10 +148,15 @@ public class RoomManager : MonoBehaviour
             beginFlood(startPosition);
         }
 
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            BeginRoomDimensionCheck();
+        }
+
 
         if (isFlooding)
         {
-            if(activeTiles <= 0)
+            if (activeTiles <= 0)
             {
                 activeTiles = 0;
                 print("RoomManager: Done flooding!");
@@ -162,7 +167,7 @@ public class RoomManager : MonoBehaviour
                 foreach (var flag in foundFlags)
                 {
                     bool wasFound = flag.Value;
-                    if(!wasFound)
+                    if (!wasFound)
                     {
                         GameObject flagObject = flag.Key;
                         print("Was unable to access " + flagObject.GetComponent<Flag>().roomName);
@@ -170,7 +175,7 @@ public class RoomManager : MonoBehaviour
                     }
                 }
 
-                if(!unreachableFlag)
+                if (!unreachableFlag)
                 {
                     print("RoomManager: Found all flags!");
                 }
@@ -185,6 +190,145 @@ public class RoomManager : MonoBehaviour
                 exploredTiles.Clear(); //reset the dictionary next time we start flooding
             }
         }
+    }
+
+    // Checks the dimensions of every room in a house scheme based on user-placed flags
+    // that denote the location of each unique room.
+    private bool BeginRoomDimensionCheck()
+    {
+        bool roomsValid = true;
+        // The following vars keep track of how many rooms are too small and too large
+        int tooSmall = 0;
+        int tooLarge = 0;
+
+        // Keep track of both flag and door objects in the scene.
+        GameObject[] flags = GameObject.FindGameObjectsWithTag("RoomFlag");
+        GameObject[] doors = GameObject.FindGameObjectsWithTag("DoorBuddy");
+
+        // Temporarily set all doors inactive to continue flood
+        foreach (GameObject door in doors)
+        {
+            door.SetActive(false);
+        }
+
+        // Flood rooms starting at each user-placed flag.
+        foreach (GameObject flag in flags)
+        {
+            Flag flagObj = flag.GetComponent<Flag>();
+            // Get position of flag in world
+            Vector3 flagPos = flag.transform.position;
+            // Convert flag position to tile position
+            Vector3Int cellPosition = tilemap.WorldToCell(flagPos);
+            RoomFlood(cellPosition);
+
+            if (exploredTiles.Count < 8)
+            {
+                tooSmall++;
+            }
+            // REPLACE WITH CURRENT HOUSE SIZE 
+            else if (exploredTiles.Count > 16000)
+            {
+                tooLarge++;
+            }
+            
+            // Clear explored tiles for subsequent room flood iteration
+            exploredTiles.Clear();
+        }
+
+        // Reactivate all of the doors in the scene for consistency
+        foreach (GameObject door in doors)
+        {
+            door.SetActive(true);
+        }
+
+
+        // Print how many rooms are too large/small and return if the house scheme is valid
+        if (tooLarge > 0)
+        {
+            Debug.Log(tooLarge + " flagged rooms are too large (must be smaller than current house size).");
+            roomsValid = false;
+        }
+
+        if (tooSmall > 0)
+        {
+            Debug.Log(tooSmall + " flagged rooms are too small (must be 8 or more tiles).");
+            roomsValid = false;
+        }
+
+        return roomsValid;
+    }
+
+    // Returns the number of tiles found in a room
+    public void RoomFlood(Vector3Int position)
+    {
+        activeTiles++;
+
+        if (tilemap.HasTile(position)) //First check if its a valid position in the tilemap
+        {
+            //Check if tile is already in dictionary. If yes, it has already been explored and should not be recounted.
+            if (exploredTiles.ContainsKey(position))
+            {
+                activeTiles--;
+                return;
+            }
+
+            // Check if there's a game object at this position in the actual coordinates
+            Vector3 worldPosition = tilemap.GetCellCenterWorld(position);
+
+            Collider2D[] colliders = Physics2D.OverlapPointAll(worldPosition);
+
+            if (colliders.Length > 0)
+            {
+                foreach (Collider2D collider in colliders)
+                {
+                    if (collider != null)
+                    {
+                        //Something is on this tile. Could be a flag, wall, object, etc.
+
+                        //Make sure it is not the room flag we're looking for
+                        if (collider.tag != "RoomFlag")
+                        {
+                            // Now it may be a furniture or wall object.
+                            //Check to see if there is a door we can go through
+                            if (collider.tag != "DoorBuddy")
+                            {
+                                //It's not a door! So leave the tile alone
+                                activeTiles--;
+                                return;
+                            }
+                            else
+                            {
+                                print("RoomManager: Encountered a door skipping through!");
+                                break;
+                            }
+                        }
+                        else //otherwise it is a room flag! So mark flag as found then continue flooding.
+                        {
+                            foundFlags[collider.gameObject] = true; //Mark the specific flag as found.
+                            print("Found " + collider.gameObject.GetComponent<Flag>().roomName);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            //Add the tile to the dictionary to mark it as explored.
+            exploredTiles.Add(position, true);
+            //colorTile(position);
+            //Mark the tile with a tint to show it has been explored
+
+            //Reminder, the position is per TILE. So go by ones, not by world coordinates.
+            //Invoke("LaunchProjectile", 2.0f);
+            RoomFlood(position + (1 * Vector3Int.up));
+            RoomFlood(position + (1 * Vector3Int.down));
+            RoomFlood(position + (1 * Vector3Int.left));
+            RoomFlood(position + (1 * Vector3Int.right));
+        }
+        else
+        {
+            //print(position + " is outside tilemap.");
+        }
+        activeTiles--;
     }
 
 }
