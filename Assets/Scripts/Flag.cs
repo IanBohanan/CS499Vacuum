6ddf853fafdbd736d8fde48df25aebfa0ef64b02
Khushal Noori <kn0045@uah.edu>
2023-12-02 13:22:50 -0600

    Create Flag.cs

---
+++ Assets/Scripts/Flag.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+//The flag for each room. Used in the housebuilder scene to make sure every room is accessible.
+//It denotes a space that's in each room
+public class Flag : MonoBehaviour
+{
+
+    public string roomName = "Room A";
+
+}
